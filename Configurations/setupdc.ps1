f56286738ce3828031a2a973f3db5109c1918850
Michael Maher <mahermick@gmail.com>
2017-08-07 18:40:32 +0100

    [Add] Help information

--- Configurations/setupdc.ps1
+++ Configurations/setupdc.ps1
@@ -1,4 +1,20 @@
-﻿Configuration DC {
+﻿<#
+
+PS C:\Users\micma\OneDrive\Scripts\Powershell\AzureRM\AzureAutomationDSC\Configurations> dc
+
+
+    Directory: C:\Users\micma\OneDrive\Scripts\Powershell\AzureRM\AzureAutomationDSC\Configurations\DC
+
+
+Mode                LastWriteTime         Length Name
+----                -------------         ------ ----
+-a----       07/08/2017     18:30           2718 localhost.mof
+WARNING: The configuration 'DC' is loading one or more built-in resources without explicitly importing associated modules. 
+Add Import-DscResource –ModuleName 'PSDesiredStateConfiguration' to your configuration to avoid this message.
+
+#>
+
+Configuration DC {
  
     WindowsFeature DHCP {
         Name = 'DHCP'
