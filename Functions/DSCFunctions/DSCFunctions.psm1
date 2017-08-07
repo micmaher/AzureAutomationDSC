8842cc1e62ac282903c5c18357965e0409847731
Michael Maher <mahermick@gmail.com>
2017-08-07 18:36:20 +0100

    [[Update] Repeat onboard DSC node

--- Functions/DSCFunctions/DSCFunctions.psm1
+++ Functions/DSCFunctions/DSCFunctions.psm1
@@ -6,10 +6,10 @@ Function Get-MmOnboardKey{
 Function Import-MmConfig{
     [CmdletBinding()]
     param(
-    [string] $ConfigName)
+    [string] $ConfigPath)
     # Imports a DSC Configuration from $workDir
     $AutoAccount = Get-AzureRMResourceGroup -Name $resourceGroup | Get-AzureRmAutomationAccount
-    $AutoAccount | Import-AzureRmAutomationDscConfiguration -SourcePath "$workDir\$ConfigName" -Published -Force
+    $AutoAccount | Import-AzureRmAutomationDscConfiguration -SourcePath "$ConfigPath" -Published -Force
     $AutoAccount | Get-AzureRmAutomationDscConfiguration -name $ConfigName
     $job = $AutoAccount | Get-AzureRmAutomationDscConfiguration -name $ConfigName | Start-AzureRmAutomationDscCompilationJob # Create the MOF file
     while (-not($job | Get-AzureRmAutomationDscCompilationJob).EndTime){Start-Sleep -Seconds 3}
